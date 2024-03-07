7877085680804c781d00015e38b2f843e5bc1f05
Jordan <jormmiller@gmail.com>
2024-03-07 13:29:02 -0500

    Added event to save to localStorage on .saveBtn click. Added funciton to fill textareas from localStorage on page load

--- assets/script.js
+++ assets/script.js
@@ -10,12 +10,23 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  $('.saveBtn').on('click', function () {
+    console.log($(this));
+    const task = $(this).siblings('textarea').val();
+    const  time = $(this).parent().attr('id');
+    localStorage.setItem(time, task);
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  $('textarea').each(function(i) {
+    const value = localStorage.getItem($(this).parent().attr('id'));
+    $(this).val(localStorage.getItem($(this).parent().attr('id')));
+  })
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
